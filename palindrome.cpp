0d1e82f4fb3e545f6334a816b07209b842ebb769
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-09-19 10:45:48 -0700

    Uncertain of fix, but stage 1 of removing randomly added chars

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,36 +1,69 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(){
-
+  
   char input[80];
+  char otherin[80];
   char clean[80];
-
-  cin.get(input, 80); 
-
   int j = 0;
-  
-  for (int i = 0; i < 80; i++) {
 
-    if ((int)input[i] >= 97 && (int)input[i] <= 122) {
-      clean[j] = input[i];
-      j++;
+  
+  cout << "Please input something. I will check if it is a palindrome." << endl;
+
+  cin.get(input, 80);
+  cin.get();
+
+  strcpy(otherin, input);
+  
+  
+  for (int i = 0; i < strlen(input); i++) {
+      if ((int)input[i] >= 97 && (int)input[i] <= 122) {
+	clean[j] = otherin[i];
+	j++;
+      }
+
+      else if ((int)input[i] >= 48 && (int)input[i] <= 57) {
+	clean[j] = otherin[i];
+	j++;
+      }
+
+      else if ((int)input[i] >= 65 && (int)input[i] <= 90) {
+	clean[j] = (char)((int)otherin[i] + 32);
+	j++;
+      }
+      else {
+	cout << "exclude: " << otherin[i];
+      }
     }
 
-    else if ((int)input[i] >= 65 && (int)input[i] <= 90) {
-      clean[j] = (char)((int)input[i] + 32);
-      j++; 
-    }
-  }
+  cout << "next" << endl;
+
+  cout << j << endl;
   
-  for (int k = 0; k < (j-1)/2; k++) {
+  for (int l = 0; l < j; ++l) {
+
+    cout << l << ":" << clean[l] << '|';
+    
+   }
+
+  cout << "next" << endl;
+
+  int full = 0;
+  for (int k = 0; k < (j - (j%2))/2; k++) {
     if (clean[k] != clean[j-k-1]) {
-      cout << "not a palindrome" << endl;
-      return 0;
+      int full = 1;
     }
   }
 
-  cout << "is a palindrome!" << endl;
+  if (full == 0) {
+  cout << "Palindrome!" << endl;
+  }
+  else if (full == 1) {
+  cout << "Not a palindrome" << endl;
+  }
+
   return 0;
 }
