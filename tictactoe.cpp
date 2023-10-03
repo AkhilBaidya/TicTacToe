7e84128a0bd1dbddd2be7bb570c3aa63ca859122
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-03 10:44:19 -0700

    made more comments

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,3 +1,11 @@
+//C++ Programming: Tic-Tac-Toe Project
+//by Akhil Baidya
+
+//Date of Submission: 10/3/2023
+
+//Notes: 
+
+//Credits: 
 
 //used https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html to reference ASCII values
 /*used suggestion from my father to use integers instead of characters throughout my code: however, this turned out
@@ -8,54 +16,64 @@ to work better after replacing cin.get() with the regular cin >>*/
 
 using namespace std;
 
-void printBoard(int board[3][3]); //change the board
+//FUNCTION PROTOTYPES (their purposes will be commented before each function):
+//changing the board:
+void printBoard(int board[3][3]); 
 void clearBoard(int board[3][3]);
 bool updateBoard(int board[3][3], int a, int b, int &turn);
 
-void playerTurn(int board[3][3], int &turn, int &p1wins, int &p2wins, int &pTies,bool &playing2); //player move
+//taking a player's move and changing the board using the above functions:
+void playerTurn(int board[3][3], int &turn, int &p1wins, int &p2wins, int &pTies,bool &round);
 
-int checkWin(int board[3][3], int turn); //check end conditions
+//checking a player's move (for win or legality)"
+int checkWin(int board[3][3], int turn);
 bool checkFull(int board[3][3]);
 bool checkLegal(int board[3][3], int a, int b);
 
+
+//MAIN FUNCTION (this is where the code will be run):
 int main(){
 
   //VARIABLES:
-  int theBoard[3][3]; //the board
+  int theBoard[3][3]; //the tic-tac-toe bard
 
-  int turn = 1; //turn #, wins
-  int p1wins = 0;
+  int turn = 1; //turn number (1 for player 1, -1 for player 2)#
+  
+  int p1wins = 0; //number of wins for each player and number of ties
   int p2wins = 0;
   int pTies = 0;
 
-  bool playing = true; //whether the game continues
-  bool playing2;
-  
-  
-  clearBoard(theBoard);
-  cout << "Let's play a game of TicTacToe!" << endl;
-  printBoard(theBoard);
+  bool game = true; //boolean determining whether the entire game continues
+  bool round; //boolean determining whether players continue putting pieces for a round (determines round end)
 
-  while (playing) {
-    clearBoard(theBoard);
-    playing2 = true;
+  //GAME FORMAT:
+  cout << "Let's play a game of TicTacToe!" << endl;
+  
+  while (game) {
+
+    clearBoard(theBoard); //at the beginning of the game, clear and display board and the round beings (round = true)
+    printBoard(theBoard);
+    round = true;
     
-    while (playing2) {
-      playerTurn(theBoard, turn, p1wins, p2wins, pTies, playing2);
+    while (round) {
+      playerTurn(theBoard, turn, p1wins, p2wins, pTies, round); //loop player turns until playerTurn changes round to false (if it detects a win or a tie)
     }
 
+    //When the round ends, shows scores:
     cout << "player 1 has " << p1wins << " wins." << endl;
     cout << "player 2 has " << p2wins << " wins." << endl;
     cout << "There have been " << pTies << " ties." << endl;
 
+    //Ask if player wants to play again (if so, game is true and the entire game loops):
     cout << "Would you like to play again? y/n" << endl;
     char answer;
     cin >> answer;
+
     if (answer == 'y') {
-      playing = true;
+      game = true;
     }
     else if (answer == 'n') {
-      playing = false;
+      game = false;
     }
   }
 
@@ -64,27 +82,27 @@ int main(){
 }
 
 
+//OTHER FUNCTIONS:
+//The printBoard function takes in the board and print outs the board (converting the integers to Xs or Os) and adds labels to the column/rows (ex. 1,2,3):
 void printBoard(int board[3][3]) {
 
-  cout << "\ta\tb\tc" << endl;
+  cout << "\ta\tb\tc" << endl; //add the column names
 
   for (int i = 0; i < 3; i++) {
-
-    cout << i+1;
     
-    for (int j = 0; j < 3; j++) {
+    cout << i+1; //add each row number
+    
+    for (int j = 0; j < 3; j++) { //for each value in board in each row...
 
       if (board[i][j] == 1) {
-      cout << "\t" << 'X';
+	cout << "\t" << 'X'; //print an X for values of 1
       }
       else if (board[i][j] == 0){
-	cout << "\t" << 'O';
+	cout << "\t" << 'O'; //print an O for values of 0
       }
       else {
-	cout << "\t" << '-';
+	cout << "\t" << '-'; //value of -1 signify empty spots
       }
-      
-      
    }
     
     cout << "\t" << endl;
@@ -93,53 +111,57 @@ void printBoard(int board[3][3]) {
   return;
 }
 
+//The clearBoard function takes in the board and replaces all values in the board with -1, signifying empty spots when the board is printed:
 void clearBoard(int board[3][3]) {
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      board[i][j] = -1;
+  for (int i = 0; i < 3; i++) {//for each row in the board...
+    for (int j = 0; j < 3; j++) {//for each value in the row...
+      board[i][j] = -1; //make the value -1
     }
   }
-
-  return;
   
+  return;
 }
 
+/*The updateBoard function takes in the board, the current turn, and some index (a,b). This index (a,b) will be the (row,column) the player, of the specified turn, places their piece. The function makes this change, if legal, and returns whether the player made an illegal move */ 
 bool updateBoard(int board[3][3], int a, int b, int &turn) {
-  int place;
 
-  cout << "a = " << a << "  b =" << b << endl;
+  int place;
   
   if (turn == 1) {
-    place = 1;
+    place = 1; //it is player 1's turn (pieces have value of 1)
   }
   else if (turn == -1) {
-    place = 0;
+    place = 0; //it is player 2's turn (pieces have value of 0)
   }  
 
-  if (checkLegal(board, a, b)) {
+  if (checkLegal(board, a, b)) { //if their desired move is legal, replace the place in the board given by (a,b) with the value of the player's piece
     board[a][b] = place;
-    return false;
+    return false; //the move was legal
   }
 
   cout << "You can't pick that spot!" << endl;
-  return true;
+  return true; //the move was illegal
 }
 
-void playerTurn(int board[3][3],int &turn, int &p1wins, int &p2wins, int &pTies, bool &playing2) {
+/*The playerTurn function takes in the board, player turn, the win and tie counts, and the boolean looping the round. The function prompts the player of the current turn to place their tac, calls other functions to place it, and sets the round to discontinue due to a win or tie. It additionally flips the turn number to the next player  (with x -1) */
+void playerTurn(int board[3][3],int &turn, int &p1wins, int &p2wins, int &pTies, bool &round) {
 
-  char loc[3];
-  bool ask = true;
+  char loc[3]; //this will contain the player's choice for placing their piece (the location)
+  bool ask = true; //this will loop the prompt of asking the player's choice if their previous choice is illegal
 
   while (ask) {
-  if (turn == 1) {
-    cout << "Player 1 (x), tell me where you want to place your tac? (ex. 2a, 1b, 3c)" << endl;
-  }
-  else {
-    cout << "Player 2 (x), tell me where you want to place your tac? (ex. 2a, 1b, 3c)" << endl;
-   }
-  
-    cin >> loc;
+
+    //ask the player for the index of where they want to place their piece:
+    if (turn == 1) {
+      cout << "Player 1 (x), tell me where you want to place your tac? (ex. 2a, 1b, 3c)" << endl;
+    }
+    else {
+      cout << "Player 2 (x), tell me where you want to place your tac? (ex. 2a, 1b, 3c)" << endl;
+    }
+    cin >> loc; //place that inputted index into the loc character array
+
+    //used ASCII 
     ask = updateBoard(board, ((int) loc[0]) -49, ((int)loc[1]) - 97, turn);
     printBoard(board);
   }
@@ -147,23 +169,23 @@ void playerTurn(int board[3][3],int &turn, int &p1wins, int &p2wins, int &pTies,
     if (checkWin(board, 1) == 1) {
 	cout << "player 1 wins!" << endl;
 	p1wins += 1;
-        playing2 = false;
+        round = false;
       }
-    else if (checkWin(board, 1) == 3) {
+    else if (checkWin(board, 1) == 2) {
 	  cout << "it's a tie!" << endl;
 	  pTies += 1;
-	  playing2 = false;
+	  round = false;
       }  
    
     else if (checkWin(board, -1) == 1) {
 	cout << "player 2 wins!" << endl;
 	p2wins += 1;
-        playing2 = false;
+        round = false;
       }
-    else if (checkWin(board, -1) == 3) {
+    else if (checkWin(board, -1) == 2) {
 	  cout << "it's a tie!" << endl;
 	  pTies += 1;
-	  playing2 = false;
+	  round = false;
       }
     
   turn *= -1;
@@ -201,10 +223,10 @@ int checkWin(int board[3][3], int turn) {
   else if (board[0][2] == lookfor && board[1][1] == lookfor && board[2][0] == lookfor) {
       return 1;
   }
- 
-  else if (checkFull(board)) {
+
+  if (checkFull(board)) {
     return 2;
-    }
+  }
 
   return 0;
 }
